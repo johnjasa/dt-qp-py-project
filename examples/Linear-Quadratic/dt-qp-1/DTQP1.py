Please see the commit message for changes, bug fixes and new features! New features - Implemented scaling and scaling test files - Improved documentation for setup classes

examples/Linear-Quadratic/dt-qp-1/DTQP1.py
@@ -18,7 +18,7 @@ from src.DTQPy_solve import DTQPy_solve
 
 opts = options()
 opts.dt.nt = 1000
-opts.solver.tolerence = 1e-5
+opts.solver.tolerence = 1e-16
 opts.solver.function = 'pyoptsparse'
 
 # time horizon
@@ -40,7 +40,7 @@ L = [LQ_objective() for n in range(5)]
 L[0].left = 1;L[0].right = 1;L[0].matrix = np.eye(2)/10
 L[1].left = 1;L[1].right = 2;L[1].matrix = np.array([[1,1,0,0],[0,0,0,0]])
 L[2].left = 2;L[2].right = 2;L[2].matrix = np.zeros((4,4));L[2].matrix[1,1] = 5
-L[3].left = 0;L[3].right = 2;L[3].matrix =np.empty((1,4),dtype = 'O'); L[3].matrix[0,1] = lambda t: -5*2*aux.g(t);L[3].matrix[0,0] = 0;L[3].matrix[0,2] = 0;L[3].matrix[0,3] = 0
+L[3].left = 0;L[3].right = 2;L[3].matrix =np.zeros((1,4),dtype = 'O'); #L[3].matrix[0,1] = lambda t: -5*2*aux.g(t);
 L[4].left = 0; L[4].right = 0;L[4].matrix =np.empty((1,1),dtype = 'O');L[4].matrix[0,0] = lambda t: 5*(aux.g(t))**2
 
 # mayer term

examples/Linear-Quadratic/fowt/Base_M1C6W6.py
@@ -116,8 +116,9 @@ from src.DTQPy_solve import DTQPy_solve
 opts = options()
 
 opts.dt.nt = 1000
-opts.solver.tolerence = 1e-6
+opts.solver.tolerence = 1e-16
 opts.solver.maxiters = 1000000
+opts.solver.function = 'pyoptsparse'
 
 time = np.linspace(tt[0],tt[-1],opts.dt.nt)
 W_pp = PchipInterpolator(np.squeeze(tt),np.squeeze(Wind_speed))
@@ -271,7 +272,7 @@ LB[2].matrix = LBs
 
 # lagrange terms
 
-R1 = 1e-8; R2 = 1e+8
+R1 = 1e-0; R2 = 1e+8
 
 lx = 0
 
@@ -313,6 +314,10 @@ L4mat = np.empty((1,1),dtype = 'O')
 L4mat[0,0] = lambda t: GP_fun(W_fun(t))
 L[lx].matrix = L4mat
 
+# 
+scale = Scaling(right = 1, matrix = np.array([1,1e-16,1e-4]))
+
+
 # setup
 s = setup()
 s.A = TVmat2cell(lambda t: A_op(W_fun(t)),time)
@@ -321,7 +326,7 @@ s.d = TVmat2cell(DXoDt_fun,time)
 s.Lagrange = L
 s.UB = UB
 s.LB = LB
-
+s.Scaling = scale
 s.t0 = 0
 s.tf = 600
 #breakpoint()

src/classes/DTQPy_CLASS_INTERNAL.py
@@ -11,37 +11,55 @@ from numpy.matlib import repmat
 from src.classes.DTQPy_CLASS_SETUP import *
 
 def DTQPy_extact_order_subsets(Otemp):
+    
+    # initialize
     Oquadratic = []
     Olinear = []
     Oconstant = []
         
     for i in range(len(Otemp)):
+            # check shape of objective array
             Otemp[i].Check_shape()
             
+            # get logical for right index
             rLogical = (Otemp[i].right>0)
+            
+            # get logical for left index
             lLogical = (Otemp[i].left>0)
-            #breakpoint()
+            
+            # combine
             IndCombined = np.array([Otemp[i].left,Otemp[i].right])
+            
+            # check for nonzero
             NonZero = np.nonzero(IndCombined)
             
+            # find logical sum
             sLogical = rLogical + lLogical
-           # breakpoint()
+ 
             if sLogical == 2:
+                
+                # Quadratic
                 Oquadratic.append(Otemp[i])
+                
             elif sLogical == 1:
+                
+                # linear objective term
                 if rLogical:
                    Ol = LQ_objective(left = 0,right = sum(IndCombined[NonZero]),matrix = Otemp[i].matrix)
                 elif lLogical:
                     Ol = LQ_objective(left = 0,right = sum(IndCombined[NonZero]),matrix = Otemp[i].matrix.T)
                 Olinear.append(Ol)
+                
             elif sLogical == 0:
+                
+                # Constant term
                 Oc = LQ_objective(left = 0,right = 0,matrix = Otemp[i].matrix)
                 Oconstant.append(Oc)
     
     return Oquadratic,Olinear,Oconstant
 
 def DTQPy_initialize(setup,dt):
-    #breakpoint()
+    
     # create empty class
     class internal:
        pass
@@ -49,10 +67,11 @@ def DTQPy_initialize(setup,dt):
     # instantiate class      
     i = internal()
     
+    # initial and final time
     i.t0 = setup.t0
     i.tf = setup.tf
     
-    #[t,w,D] = DTQPy_MESH_pts(i,dt)
+    #create mesh
     t = np.linspace(i.t0,i.tf,dt.nt)
     t = t[None].T
     w = []; D = []
@@ -60,17 +79,19 @@ def DTQPy_initialize(setup,dt):
     i.t = t;i.w = w;i.D = D;
     h = np.diff(t,axis=0);i.h = h
     
-    # needs to be updated
+    # mid points
     i.tm = np.array([])
     
-    # number of controls, states and parameters
+    # get the number of controls, states and parameters
     ny = max([np.shape(setup.A)[0],np.shape(setup.B)[0],np.shape(setup.G)[0],np.shape(setup.d)[0]]);i.ny = ny
     nu = np.shape(setup.B)[1];i.nu = nu
     npl = np.shape(setup.G)[1];i.npl = npl
     nd = np.shape(setup.d)[1];i.nd = nd
     
+    # total number of optimization variables
     nx = (nu+ny)*nt + npl; i.nx = nx
     
+    # initialize dummy A matrix
     if np.size(setup.A) == 0:
         setup.A = np.zeros((ny,ny))
         
@@ -89,21 +110,32 @@ def DTQPy_initialize(setup,dt):
     i.IN = IN
     i.I_stored = I_stored
     
+    # auxdata
     i.auxdata = setup.auxdata
     
     # lagrange and mayer terms
-    #breakpoint()
+    
+    # get Lagrange terms
     Ltemp = setup.Lagrange
-    #breakpoint()
+    
+    # get quadratic, linear and constant terms
     Lquadratic,Llinear,Lconstant = DTQPy_extact_order_subsets(Ltemp)
     
+    # get Mayer term
     Mtemp = setup.Mayer
+    
+    # get quadratic, linear and constant terms
     Mquadratic,Mlinear,Mconstant = DTQPy_extact_order_subsets(Mtemp)
     
-    #breakpoint()
-        
+     # add to setup  
     setup.Lquadratic = Lquadratic; setup.Llinear = Llinear; setup.Lconstant = Lconstant
     setup.Mquadratic = Mquadratic; setup.Mlinear = Mlinear; setup.Mconstant = Mconstant
-    #breakpoint()
+    
+    # Scaling terms
+    lenScale = len(setup.Scaling)
+    if lenScale !=0:
+        for k in range(lenScale):
+            
+            setup.Scaling[k].Check_Fields(i)
  
     return setup,i

src/solver/DTQPy_SOLVER_osqp.py
@@ -12,11 +12,15 @@ import numpy as np
 
 def DTQPy_SOLVER_osqp(H,f,A,b,Aeq,beq,lb,ub,internal,opts):
     
+    # obtain solver options
     solver = opts.solver
     
+    # set options
     options = {'eps_abs':solver.tolerence,'eps_rel': solver.tolerence,'max_iter':solver.maxiters}
     
-    
+    # Construct the problem in the way osqp needs it
+    # min 1/2*x'*H*x + q'*x
+    # subto: l < Ax < u
     Al = sparse.vstack([A,Aeq,sparse.eye(internal.nx)]); Al =  Al.tocsc()
     lbL = np.vstack([b.todense(),beq.todense(),lb[None].T])
     ubL = np.vstack([b.todense(),beq.todense(),ub[None].T])
@@ -42,7 +46,7 @@ def DTQPy_SOLVER_osqp(H,f,A,b,Aeq,beq,lb,ub,internal,opts):
         F = None
     else:
         F = res.info.obj_val
-    breakpoint()
+    
     return X,F,internal,opts
     
     

src/utilities/DTQPy_tmatrix.py
@@ -12,13 +12,19 @@ import types
 
 
 def DTQPy_tmatrix(A,p,*args):
-    #breakpoint()
+    
     
     # check if another time mesh is imputted
     if len(args) !=0:
         t = args[0]
     else:
         t = p.t
+        
+    # convert lambda function to a np array of type object
+    if type(A) is types.LambdaType:
+            Ae = np.empty((1,1),dtype ='O')
+            Ae[0,0] = A
+            A = Ae
     
     # if A is empty 
     if len(A)==0:
@@ -48,18 +54,18 @@ def DTQPy_tmatrix(A,p,*args):
                 elif type(A[i,j]) is types.LambdaType: # A is time varying
                     if A[i,j].__code__.co_argcount == 2:
                           At[:,i,j] = np.ravel(A[i,j](t,p))
+                          
                     elif A[i,j].__code__.co_argcount == 1:
-                        #breakpoint()
                         At[:,i,j] = np.ravel(A[i,j](t))
+                        
                     else:
                         raise ValueError("not a properly defined function")
+                        
                 elif A[i,j] == 0:
                     pass
                 else:
                     At[:,i,j] = A[i,j] # A is time invariant
                     
-                    
-   
     return At    
                 
         